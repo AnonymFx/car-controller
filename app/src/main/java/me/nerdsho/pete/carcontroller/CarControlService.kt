1579a804a54bbef1eec881ac439436a8639ff60e
Thomas Pettinger <pettinger@cqse.eu>
2018-12-28 22:38:56 +0100

    First steering prototype working

--- app/src/main/java/me/nerdsho/pete/carcontroller/CarControlService.kt
+++ app/src/main/java/me/nerdsho/pete/carcontroller/CarControlService.kt
@@ -1,24 +1,29 @@
 package me.nerdsho.pete.carcontroller
 
-import android.app.Service
+import android.app.*
 import android.bluetooth.*
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Binder
 import android.os.Handler
 import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
 
-class CarControllService: Service() {
-
+class CarControlService : Service() {
     companion object {
         private const val ENABLE_BLUETOOTH_REQUEST = 0
         private const val CAR_CONTROLLER_MAC = "FB:F4:20:A6:08:40"
         private const val CAR_CONTROL_SERVICE_UUID = "00001234-0000-1000-8000-00805f9b34fb"
         private const val CAR_CONTROL_WRITE_CHARACTERISTIC_UUID = "00001235-0000-1000-8000-00805f9b34fb"
         private val SCAN_PERIOD: Long = 10000
+        private val ONGOING_NOTIFICATION_ID = 1
     }
 
+    private val binder = CarControlBinder()
     private val bluetoothAdapter by lazy(LazyThreadSafetyMode.NONE) {
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothManager.adapter
@@ -31,10 +36,12 @@ class CarControllService: Service() {
         override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
             when (newState) {
                 BluetoothProfile.STATE_CONNECTED -> {
-                    this@CarControllService.gatt = gatt
-                    this@CarControllService.gatt?.discoverServices()
+                    this@CarControlService.gatt = gatt
+                    this@CarControlService.gatt?.discoverServices()
                 }
                 BluetoothProfile.STATE_DISCONNECTED -> {
+                    this@CarControlService.gatt = null
+                    this@CarControlService.carControl = null
                 }
             }
         }
@@ -54,6 +61,8 @@ class CarControllService: Service() {
                     service.uuid.toString() == CAR_CONTROL_SERVICE_UUID
                 }
                     ?.characteristics?.find { characteristic -> characteristic.uuid.toString() == CAR_CONTROL_WRITE_CHARACTERISTIC_UUID }
+                carControl?.setValue("5\n")
+                gatt.writeCharacteristic(carControl)
             }
         }
     }
@@ -62,7 +71,7 @@ class CarControllService: Service() {
             super.onScanResult(callbackType, result)
             if (result?.device?.address == CAR_CONTROLLER_MAC) {
                 val carController = result.device
-                carController?.connectGatt(this@CarControllService, false, connectGattCallback)
+                carController?.connectGatt(this@CarControlService, false, connectGattCallback)
             }
         }
 
@@ -73,11 +82,37 @@ class CarControllService: Service() {
 
     override fun onCreate() {
         super.onCreate()
+        Toast.makeText(this, "Car control service starting...", Toast.LENGTH_SHORT).show()
+
+        val channelId = createNotificationChannel("car_control_service", "Car control service")
+
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(this, 0, notificationIntent, 0)
+            }
+
+        val notification: Notification = Notification.Builder(this, channelId)
+            .setContentTitle("Car control service")
+            .setContentIntent(pendingIntent)
+            .build()
+
+        startForeground(ONGOING_NOTIFICATION_ID, notification)
+
         scanLeDevice(true)
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "Car control service stopping...", Toast.LENGTH_SHORT).show()
+    }
+
+//    override fun onHandleIntent(intent: Intent?) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        Toast.makeText(this, "Received intent", Toast.LENGTH_SHORT).show()
+//    }
+
     override fun onBind(intent: Intent?): IBinder? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return binder
     }
 
     private fun scanLeDevice(enable: Boolean) {
@@ -95,5 +130,30 @@ class CarControllService: Service() {
                 bluetoothAdapter.bluetoothLeScanner.stopScan(scanCallback)
             }
         }
+    }
+
+    private fun createNotificationChannel(channelId: String, channelName: String): String {
+        val chan = NotificationChannel(
+            channelId,
+            channelName, NotificationManager.IMPORTANCE_NONE
+        )
+        chan.lightColor = Color.BLUE
+        chan.lockscreenVisibility = Notification.VISIBILITY_PRIVATE
+        val service = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        service.createNotificationChannel(chan)
+        return channelId
+    }
+
+    fun sendCommand(command: String) {
+        if (carControl == null) {
+            scanLeDevice(true)
+        }
+        carControl?.setValue("$command\n")
+        val status = gatt?.writeCharacteristic(carControl)
+        Log.d("SentCommand", "Sent $command to characteristic $carControl with status $status")
+    }
+
+    inner class CarControlBinder : Binder() {
+        fun getService(): CarControlService = this@CarControlService
     }
 }
--- app/src/main/java/me/nerdsho/pete/carcontroller/MainActivity.kt
+++ app/src/main/java/me/nerdsho/pete/carcontroller/MainActivity.kt
@@ -3,127 +3,101 @@ package me.nerdsho.pete.carcontroller
 import android.bluetooth.*
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
+import android.os.IBinder
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import java.lang.Thread.sleep
+import java.util.concurrent.Future
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        private const val ENABLE_BLUETOOTH_REQUEST = 0
-        private const val CAR_CONTROLLER_MAC = "FB:F4:20:A6:08:40"
-        private const val CAR_CONTROL_SERVICE_UUID = "00001234-0000-1000-8000-00805f9b34fb"
-        private const val CAR_CONTROL_WRITE_CHARACTERISTIC_UUID = "00001235-0000-1000-8000-00805f9b34fb"
-        private val SCAN_PERIOD: Long = 10000
-    }
+    private var carControlService: CarControlService? = null
+    private var sendingTask: Future<Unit>? = null
+    private val serviceConnection = object : ServiceConnection {
 
-    private val bluetoothAdapter by lazy(LazyThreadSafetyMode.NONE) {
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothManager.adapter
-    }
-    private val handler = Handler()
-    private var isScanning = false
-    private var carControl: BluetoothGattCharacteristic? = null
-    private var gatt: BluetoothGatt? = null
-    private var connectGattCallback = object : BluetoothGattCallback() {
-        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
-            when (newState) {
-                BluetoothProfile.STATE_CONNECTED -> {
-                    log("Connected to gatt $gatt, status $status")
-                    this@MainActivity.gatt = gatt
-                    this@MainActivity.gatt?.discoverServices()
-                }
-                BluetoothProfile.STATE_DISCONNECTED -> {
-                    log("Disconnected from gatt $gatt, status $status")
-                }
-            }
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            val binder = service as CarControlService.CarControlBinder
+            carControlService = binder.getService()
         }
 
-        override fun onCharacteristicWrite(
-            gatt: BluetoothGatt?,
-            characteristic: BluetoothGattCharacteristic?,
-            status: Int
-        ) {
-            super.onCharacteristicWrite(gatt, characteristic, status)
-            log("OnCharacteristicWrite with status $status")
-        }
-
-        // New services discovered
-        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
-            log("Service discovered with status $status")
-            if (carControl == null) {
-//                gatt.services.find { service ->
-//                    service.uuid.toString() == CAR_CONTROL_SERVICE_UUID
-//                }?.characteristics?.forEach { char -> Log.d("blub", char.uuid.toString())}
-                carControl = gatt.services.find { service ->
-                    service.uuid.toString() == CAR_CONTROL_SERVICE_UUID
-                }
-                    ?.characteristics?.find { characteristic -> characteristic.uuid.toString() == CAR_CONTROL_WRITE_CHARACTERISTIC_UUID }
-            }
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            carControlService = null
         }
     }
-    private val scanCallback = object : ScanCallback() {
-        override fun onScanResult(callbackType: Int, result: ScanResult?) {
-            super.onScanResult(callbackType, result)
-            if (result?.device?.address == CAR_CONTROLLER_MAC) {
-                val carController = result.device
-                carController?.connectGatt(this@MainActivity, false, connectGattCallback)
-            }
-        }
-
-        override fun onScanFailed(errorCode: Int) {
-            super.onScanFailed(errorCode)
-            log("Error code $errorCode when scanning for BLE devices")
-        }
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        scanLeDevice(true)
+        Intent(this, CarControlService::class.java).also { intent -> startForegroundService(intent) }
+        motor_control.progress = motor_control.max/2
+        steering_control.progress = steering_control.max/2
+
+//        scanLeDevice(true)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, CarControlService::class.java).also { intent ->
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        }
     }
 
     override fun onStop() {
         super.onStop()
-        gatt?.close()
-        carControl = null
+        unbindService(serviceConnection)
+        carControlService = null
+        sendingTask?.cancel(true)
+        sendingTask = null
     }
 
-    private fun scanLeDevice(enable: Boolean) {
-        when (enable) {
-            true -> {
-                handler.postDelayed({
-                    isScanning = false
-                    bluetoothAdapter.bluetoothLeScanner.stopScan(scanCallback)
-                }, SCAN_PERIOD)
-                isScanning = true
-                bluetoothAdapter.bluetoothLeScanner.startScan(scanCallback)
-            }
-            else -> {
-                isScanning = false
-                bluetoothAdapter.bluetoothLeScanner.stopScan(scanCallback)
+    private fun startBackgroundTask(): Future<Unit> {
+        return doAsync {
+            while (true) {
+//                Log.d("motor_control", (motor_control.progress - 100).toString())
+//                Log.d("steering_control", (steering_control.progress - 100).toString())
+                sleep(100)
+                carControlService?.sendCommand("m${motor_control.progress - motor_control.max/2}s${-(steering_control.progress - steering_control.max/2)}")
             }
         }
     }
 
-    private fun log(logEntry: String) {
-        runOnUiThread { log_view.text = "$logEntry\n\n${log_view.text}" }
-    }
+//    private fun log(logEntry: String) {
+//        runOnUiThread { log_view.text = "$logEntry\n\n${log_view.text}" }
+//    }
 
     fun onClickSend(view: View) {
-        val command = "${command_view.text}\n"
-        log("Sending command: ${command_view.text}")
+        val command = "${command_view.text}"
+        carControlService?.sendCommand(command)
+    }
 
-        log("carController is $carControl")
+    fun onClickStop(view: View) {
+        motor_control.progress = motor_control.max/2
+        steering_control.progress = steering_control.max/2
+        sendingTask?.cancel(true)
+        sendingTask = null
+    }
 
-        carControl?.setValue(command)
-        log("Characteristic value is ${String(carControl!!.value)}")
+    fun onClickStart(view: View) {
+        sendingTask = startBackgroundTask()
+    }
 
-        val status = gatt?.writeCharacteristic(carControl)
-        log("Wrote characteristic with status $status")
+    fun onClickRestartService(view: View) {
+        stopService(Intent(this, CarControlService::class.java))
+        Intent(this, CarControlService::class.java).also { intent -> startForegroundService(intent) }
+        Intent(this, CarControlService::class.java).also { intent ->
+            bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE)
+        }
     }
 }
